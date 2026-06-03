cddda6206988366b6b8e6cd2daac960945c46c9f
Iurii Dubinin <yuradubinin100@gmail.com>
2026-06-03 16:22:55 +0300

    RequestValidator: upd

--- src/utils/Utils.h
+++ src/utils/Utils.h
@@ -31,23 +31,7 @@ namespace utils {
 
     std::set<std::string> SplitToSet(const std::string& str);
 
-    std::string Trim(const std::string& str) {
-        const auto begin = str.find_first_not_of(" \t");
-        if (begin == std::string::npos)
-            return "";
-        const auto end = str.find_last_not_of(" \t");
-        return str.substr(begin, end - begin + 1);
-    }
+    std::string Trim(const std::string& str);
 
-    std::set<std::string> SplitToSet(const std::string& str) {
-        std::set<std::string> out;
-        std::stringstream     ss(str);
-        std::string           item;
-        while (std::getline(ss, item, ',')) {
-            const std::string trimmed = Trim(item);
-            if (!trimmed.empty())
-                out.insert(trimmed);
-        }
-        return out;
-    }
+    std::set<std::string> SplitToSet(const std::string& str);
 } // namespace utils
